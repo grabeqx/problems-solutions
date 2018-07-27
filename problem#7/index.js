d31921d1a5c8bd30dce762095f27e392c0aa3144
grabeqx <grabowski.kontakt@gmail.com>
2018-07-27 22:33:43 +0200

    Problem 7

---
+++ problem#7/index.js
@@ -0,0 +1,33 @@
+const alphabet = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    r: 17,
+    s: 18,
+    t: 19,
+    u: 20,
+    q: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+};
+
+
+function getAllWays(number) {
+    // to be done
+};
