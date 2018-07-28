d638c118c8ae9e3f78086c9506821a22f5aeb354
grabeqx <grabowski.kontakt@gmail.com>
2018-07-28 19:11:16 +0200

    Problem 7

--- problem#7/index.js
+++ problem#7/index.js
@@ -1,33 +1,30 @@
-const alphabet = {
-    a: 1,
-    b: 2,
-    c: 3,
-    d: 4,
-    e: 5,
-    f: 6,
-    g: 7,
-    h: 8,
-    i: 9,
-    j: 10,
-    k: 11,
-    l: 12,
-    m: 13,
-    n: 14,
-    o: 15,
-    p: 16,
-    r: 17,
-    s: 18,
-    t: 19,
-    u: 20,
-    q: 21,
-    v: 22,
-    w: 23,
-    x: 24,
-    y: 25,
-    z: 26
-};
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','r','s','t','u','q','v','w','x','y','z'];
 
 
 function getAllWays(number) {
-    // to be done
+
+    var encoded = number.toString();
+    encoded = [...encoded];
+    var one = '',
+        two = '',
+        longerNumber = [],
+        decoded = [];
+    console.log(encoded);
+    for(var i in encoded) {
+        if(parseInt(encoded[i]) > 0) {
+            one += alphabet[encoded[i] -1];
+            longerNumber.push(encoded[i]);
+            if(longerNumber.length == 2) {
+                two += alphabet[longerNumber.join('') - 1];
+            }
+        }
+    }
+    
+    decoded.push(one);
+    decoded.push(two);
+    return decoded;
+
 };
+
+var decoded = getAllWays(112);
+console.log(decoded);
